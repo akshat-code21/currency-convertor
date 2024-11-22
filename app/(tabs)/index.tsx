7ebb1acb90457c7d3360a5c35fa0a89dae368530
Akshat Sipany <borncancer21@gmail.com>
2024-11-22 19:23:49 +0530

    v1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,134 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Animated,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useRef } from "react";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function Index() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.95)).current;
 
-export default function HomeScreen() {
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.spring(scaleAnim, {
+        toValue: 1,
+        tension: 20,
+        friction: 7,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topDecoration} />
+      <Animated.View
+        style={[
+          styles.mainContent,
+          {
+            opacity: fadeAnim,
+            transform: [{ scale: scaleAnim }],
+          },
+        ]}
+      >
+        <Text style={styles.heading}>Welcome</Text>
+        <Text style={styles.subheading}>to Currency Convertor</Text>
+        <View style={styles.cardContainer}>
+          <TouchableOpacity style={styles.card}>
+            <View style={styles.cardContent}>
+              <View style={styles.dot} />
+              <Text style={styles.cardText}>
+                Navigate to the main page using the tab below.
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
+      <View style={styles.bottomDecoration} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
   },
-  stepContainer: {
-    gap: 8,
+  topDecoration: {
+    height: 150,
+    backgroundColor: "#FF6B6B",
+    borderBottomLeftRadius: 30,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    left: 0,
+  },
+  bottomDecoration: {
+    height: 200,
+    backgroundColor: "#4ECDC4",
+    borderTopRightRadius: 30,
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    opacity: 0.7,
+  },
+  mainContent: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 42,
+    fontWeight: "800",
+    color: "#2C3E50",
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  subheading: {
+    fontSize: 24,
+    color: "#34495E",
+    marginBottom: 40,
+    fontWeight: "300",
+  },
+  cardContainer: {
+    marginTop: 20,
+  },
+  card: {
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  cardContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "#4ECDC4",
+    marginRight: 10,
+  },
+  cardText: {
+    fontSize: 18,
+    color: "#2C3E50",
+    fontWeight: "500",
   },
 });
---
+++ app/(tabs)/main.tsx
@@ -0,0 +1,354 @@
+import { Ionicons } from "@expo/vector-icons";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  Alert,
+  Dimensions,
+} from "react-native";
+import SelectDropdown from "react-native-select-dropdown";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { SafeAreaView, TextInput } from "react-native";
+import {
+  createTamagui,
+  TamaguiProvider,
+  View,
+  XStack,
+  YStack,
+  Button,
+} from "tamagui";
+import defaultConfig from "@tamagui/config/v3";
+
+const { width, height } = Dimensions.get("window");
+const config = createTamagui(defaultConfig);
+
+export default function App() {
+  const baseDropdownRef = useRef(null);
+  const finalDropdownRef = useRef(null);
+
+  const [options, setOptions] = useState<
+    { code: string; description: string }[]
+  >([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [baseAmount, setBaseAmount] = useState("");
+  const [finalAmount, setFinalAmount] = useState("");
+  const [baseCurrency, setBaseCurrency] = useState<{
+    code: string;
+    description: string;
+  } | null>(null);
+  const [finalCurrency, setFinalCurrency] = useState<{
+    code: string;
+    description: string;
+  } | null>(null);
+
+  const getOptions = async () => {
+    try {
+      const response = await fetch(
+        "https://v6.exchangerate-api.com/v6/e62dba6edb2f60423c5dcedb/codes"
+      );
+      const json: { supported_codes: [string, string][] } =
+        await response.json();
+
+      const formattedOptions = json.supported_codes.map(
+        ([code, description]) => ({
+          code,
+          description,
+        })
+      );
+
+      setOptions(formattedOptions);
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error", "Failed to load currency options");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const getFinalPrice = async () => {
+    if (!baseCurrency || !finalCurrency || !baseAmount) {
+      Alert.alert("Error", "Please select currencies and enter an amount");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/e62dba6edb2f60423c5dcedb/pair/${baseCurrency.code}/${finalCurrency.code}/${baseAmount}`
+      );
+      const json = await response.json();
+
+      const convertedAmount = json.conversion_result.toFixed(2).toString();
+      setFinalAmount(convertedAmount);
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Error", "Failed to convert currency");
+    }
+  };
+
+  const handleSwapCurrencies = () => {
+    if (!baseCurrency || !finalCurrency) return;
+
+    const newBaseCurrency = finalCurrency;
+    const newFinalCurrency = baseCurrency;
+
+    setBaseCurrency(newBaseCurrency);
+    setFinalCurrency(newFinalCurrency);
+
+    const baseIndex = options.findIndex(
+      (item) => item.code === newBaseCurrency.code
+    );
+    const finalIndex = options.findIndex(
+      (item) => item.code === newFinalCurrency.code
+    );
+
+    if (baseDropdownRef.current && finalDropdownRef.current) {
+      // @ts-ignore
+      baseDropdownRef.current.selectIndex(baseIndex);
+      // @ts-ignore
+      finalDropdownRef.current.selectIndex(finalIndex);
+    }
+
+    getFinalPrice();
+  };
+
+  useEffect(() => {
+    getOptions();
+  }, []);
+
+  useEffect(() => {
+    if (baseCurrency && finalCurrency && baseAmount) {
+      getFinalPrice();
+    }
+  }, [baseCurrency, finalCurrency, baseAmount]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text style={styles.loadingText}>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <TamaguiProvider config={config}>
+      <View style={styles.container}>
+        <YStack
+          space="$4"
+          width={width * 0.9}
+          backgroundColor="#f0f4f7"
+          borderRadius={20}
+          padding={20}
+          elevation={5}
+        >
+          <Text style={styles.title}>Currency Converter</Text>
+
+          <YStack space="$3">
+            <TextInput
+              placeholder="Enter Amount"
+              placeholderTextColor={"#6b7280"}
+              value={baseAmount}
+              onChangeText={(text) => {
+                const numericText = text.replace(/[^0-9.]/g, "");
+                setBaseAmount(numericText);
+              }}
+              keyboardType="numeric"
+              style={styles.input}
+            />
+
+            <SelectDropdown
+              ref={baseDropdownRef}
+              data={options}
+              onSelect={(selectedItem) => {
+                setBaseCurrency(selectedItem);
+              }}
+              renderButton={(selectedItem, isOpened) => (
+                <View style={styles.dropdownButton}>
+                  <Text style={styles.dropdownButtonText}>
+                    {selectedItem
+                      ? `${selectedItem.code} - ${selectedItem.description}`
+                      : "Select Base Currency"}
+                  </Text>
+                  <Icon
+                    name={isOpened ? "expand-less" : "expand-more"}
+                    style={styles.dropdownButtonIcon}
+                  />
+                </View>
+              )}
+              renderItem={(item, _, isSelected) => (
+                <View
+                  style={[
+                    styles.dropdownItem,
+                    isSelected && styles.selectedDropdownItem,
+                  ]}
+                >
+                  <Text style={styles.dropdownItemText}>
+                    {item.code} - {item.description}
+                  </Text>
+                </View>
+              )}
+              dropdownStyle={styles.dropdownMenu}
+            />
+          </YStack>
+
+          <XStack
+            justifyContent="center"
+            alignItems="center"
+            marginVertical={10}
+          >
+            <Button
+              circular
+              backgroundColor="#e0e7f0"
+              onPress={handleSwapCurrencies}
+            >
+              <Ionicons name="swap-vertical" size={24} color="#333" />
+            </Button>
+          </XStack>
+
+          <YStack space="$3">
+            <SelectDropdown
+              ref={finalDropdownRef}
+              data={options}
+              onSelect={(selectedItem) => {
+                setFinalCurrency(selectedItem);
+              }}
+              renderButton={(selectedItem, isOpened) => (
+                <View style={styles.dropdownButton}>
+                  <Text style={styles.dropdownButtonText}>
+                    {selectedItem
+                      ? `${selectedItem.code} - ${selectedItem.description}`
+                      : "Select Final Currency"}
+                  </Text>
+                  <Icon
+                    name={isOpened ? "expand-less" : "expand-more"}
+                    style={styles.dropdownButtonIcon}
+                  />
+                </View>
+              )}
+              renderItem={(item, _, isSelected) => (
+                <View
+                  style={[
+                    styles.dropdownItem,
+                    isSelected && styles.selectedDropdownItem,
+                  ]}
+                >
+                  <Text style={styles.dropdownItemText}>
+                    {item.code} - {item.description}
+                  </Text>
+                </View>
+              )}
+              dropdownStyle={styles.dropdownMenu}
+            />
+
+            <TextInput
+              value={finalAmount}
+              editable={false}
+              style={styles.resultInput}
+              placeholder="Converted Amount"
+              placeholderTextColor="#6b7280"
+            />
+          </YStack>
+
+          <Button
+            themeInverse
+            size="$6"
+            backgroundColor="#4a90e2"
+            onPress={getFinalPrice}
+            borderRadius={15}
+          >
+            <Text style={styles.convertButtonText}>Convert</Text>
+          </Button>
+        </YStack>
+      </View>
+    </TamaguiProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f4f7",
+  },
+  loadingText: {
+    color: "#333",
+    marginTop: 10,
+    fontSize: 16,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f4f7",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 15,
+    fontSize: 18,
+    color: "#333",
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+  },
+  resultInput: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 15,
+    fontSize: 18,
+    color: "#4a90e2",
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+  },
+  dropdownButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+  },
+  dropdownButtonText: {
+    color: "#333",
+    fontSize: 16,
+  },
+  dropdownButtonIcon: {
+    color: "#333",
+    fontSize: 24,
+  },
+  dropdownMenu: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+  },
+  dropdownItem: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e0e0e0",
+  },
+  selectedDropdownItem: {
+    backgroundColor: "#f0f0f0",
+  },
+  dropdownItemText: {
+    fontSize: 16,
+    color: "#333",
+  },
+  convertButtonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
